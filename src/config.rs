42ba49c82b68d11e98e89df8d78f1d803bd88321
QUEVAL Martin <116591403+MartinQueval@users.noreply.github.com>
2025-02-13 11:19:35 -0500

    config file management v1

---
+++ src/config.rs
@@ -0,0 +1,47 @@
+use serde::Deserialize;
+use std::fs;
+use std::path::Path;
+
+/**
+ * @brief Define a struct to represent the configuration file.
+ * It contains the operating system, the reboot time, the RAM clearing frequency,
+ * and the list of services  that should be restarted after a reboot.
+ */
+
+//Constructor
+#[derive(Debug, Deserialize)]
+pub struct Config {
+    pub os: String,
+    pub restart_hour: String,
+    pub cache_clear_interval: u32,
+    pub services: Vec<String>,
+}
+
+// Implement Config
+impl Config {
+    /**
+    * @brief Load the configuration from the configuration file
+    * @param String Path to file
+    * @returns config the struct of the config file
+    */
+    pub fn load(path: &str) -> Result<Self, Box<dyn std::error::Error>> {
+        //Load the configuration from the configuration file
+        let content = fs::read_to_string(Path::new(path))?;
+
+        // Parse the contents of the file into a Config object
+        let config: Config = toml::from_str(&content)?;
+
+        // Return the configuration
+        Ok(config)
+    }
+
+    /**
+    * @brief Print the information from the configuration file
+    */
+    pub fn show_config(&self) {
+        println!("OS: {}", self.os);
+        println!("Reboot time: {}", self.restart_hour);
+        println!("Cache clear interval: {}", self.cache_clear_interval);
+        println!("Services: {:?}", self.services);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+mod config;
+
+/**
+ * @brief Test the configuration management system, basically prints the config file.
+ */
+
+// Relative path to the config file
+const CONFIG_PATH: &str = "config.toml";
+
+//Main
 fn main() {
-    println!("Hello, world!");
+    // Load the config file
+    let config = config::Config::load(CONFIG_PATH)
+        .expect("Impossible de charger la configuration");
+
+    // Print the configuration details
+    config.show_config();
 }
